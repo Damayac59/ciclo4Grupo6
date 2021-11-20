f99f1a730e32ecfa65309ec4cc138c18a60f3155
Damayac59 <Damaya59@gmail.com>
2021-11-20 09:45:42 -0500

    Creación carpeta Daniel_Amaya_Cristancho con componente Informe_Usuario

---
+++ Daniel_Amaya_Cristancho/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { InformeEmpleadosComponent } from './informe-empleados/informe-empleados.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InformeEmpleadosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Daniel_Amaya_Cristancho/src/app/informe-empleados/informe-empleados.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-informe-empleados',
+  templateUrl: './informe-empleados.component.html',
+  styleUrls: ['./informe-empleados.component.css']
+})
+export class InformeEmpleadosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
