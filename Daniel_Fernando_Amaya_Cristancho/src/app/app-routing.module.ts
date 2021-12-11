ecced7dd50b1e95dc4c156f3c378d388fe37dadf
Damayac59 <Damaya59@gmail.com>
2021-12-10 23:55:38 -0500

    ajuste routerlink
    Routerlink administrador

--- Daniel_Fernando_Amaya_Cristancho/src/app/app-routing.module.ts
+++ Daniel_Fernando_Amaya_Cristancho/src/app/app-routing.module.ts
@@ -3,7 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { PaginaPrincipalComponent } from './daniel/pagina-principal/pagina-principal.component'
 import { InformeEmpleadosComponent } from './daniel/informe-empleados/informe-empleados.component'
 const routes: Routes = [
+  {path: '', component:PaginaPrincipalComponent},
   {path: 'pagina_principal', component:PaginaPrincipalComponent},
+  {path: 'informe_empleados', component:InformeEmpleadosComponent},
 
 ];
 
